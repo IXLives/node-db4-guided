10baf108c11a90160fb69d39d89f02f3489ae1e0
Ian Bryant <48700036+IXLives@users.noreply.github.com>
2019-08-28 21:33:03 -0400

    Cleanup Fixed

--- data/seeds/00-cleanup.js
+++ data/seeds/00-cleanup.js
@@ -2,6 +2,8 @@ const cleaner = require("knex-cleaner");
 
 exports.seed = function(knex) {
   return cleaner.clean(knex, {
-    ignoreTables: ["knex_migrations", "knex_migrations_lock"] // don't empty migration tables
+    // add this line
+    mode: 'truncate',
+    ignoreTables: ['knex_migrations', 'knex_migrations_lock'], // don't empty migration tables
   });
 };
